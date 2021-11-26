7fea50e34df6a1ceb5b174a6e8e736609dd2414d
ValerioMik <valerio.avallone@hotmail.com>
2021-11-26 17:57:58 +0100

    buon punto card quasi stampate

--- src/components/Contenitorepadre.vue
+++ src/components/Contenitorepadre.vue
@@ -1,32 +1,57 @@
 <template>
- <div id="contenitorecard">
-     <div class="centratura">
-         <Cards/>
-     </div>
-
- </div>
+  <div id="contenitorecard">
+    <div class="centratura">
+      <Cards :v-for="canzoni,i in listacanzoni"
+      :kei="i"
+      :details="canzoni"/>
+      
+    </div>
+  </div>
 </template>
 
 <script>
-import Cards from '@/components/Cards.vue'
+import axios from "axios";
+import Cards from "@/components/Cards.vue";
 
 export default {
-  name: 'Contenitorepadre',
+  name: "Contenitorepadre",
   components: {
-    Cards
-  }
-  
-}
+    Cards,
+  },
+  data() {
+    return {
+      apiUrl: "https://flynn.boolean.careers/exercises/api/array/music",
+      listacanzoni: [],
+    };
+  },
+  created() {
+    this.getcaracters();
+  },
+  methods: {
+    getcaracters() {
+      //console.log("ho finito una parte");
+      axios
+      .get(this.apiUrl)
+      .then((result) => {
+        this.listacanzoni = result.data
+        //console.log(result.data);
+      });
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-#contenitorecard{
-    background-color: rgb(30, 45, 59);
-    height: 600px;
+#contenitorecard {
+  background-color: rgb(30, 45, 59);
+  height: 600px;
 }
-.centratura{
-    width: 80%;
-    margin: 0 auto;
+.centratura {
+  width: 70%;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 }
 </style>
